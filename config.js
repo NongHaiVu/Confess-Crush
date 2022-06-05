b232ed779ad9003f1389e6e54cddd44809c83640
Nguoi LA <106913248+NongHaiVu@users.noreply.github.com>
2022-06-05 19:24:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi Lâm Thị Lệ Quyên",
+    introTitle: 'Quyên',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    title: 'Em ơi em à Vũ muốn ăn em quáa 🥰',
+    desc: 'Cho Vũ đớp miếng nhaa',
+    btnYes: 'Dạ<33',
+    btnNo: 'Không nha:3',
+    question: 'Thế là đồng ý cho Vũ ăn rùi nhaaa<3',
+    btnReply: 'aaa cho Vũ ăn em luôn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Vũ biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'hehe chuẩn bị đớp em thuii.',
+    btnAccept: 'Dạ <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
